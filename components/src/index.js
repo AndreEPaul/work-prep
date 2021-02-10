0e170410b8762f6d84b9b5f2dcbafe2d14b72aa0
Andre Paul <andrepaul8128@gmail.com>
2021-02-09 16:28:41 -0800

    prettier

--- components/src/index.js
+++ components/src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
-import faker from 'faker';
+import faker from "faker";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:00PM"
-                commentText="Nice blog post!"
-                myAvatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:20PM"
-                commentText="I don't like this."
-                myAvatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 9:35AM"
-                commentText="Where am I?"
-                myAvatar={faker.image.image()}
-            />
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:00PM"
+        commentText="Nice blog post!"
+        myAvatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:20PM"
+        commentText="I don't like this."
+        myAvatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 9:35AM"
+        commentText="Where am I?"
+        myAvatar={faker.image.image()}
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
