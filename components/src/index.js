069a0286a090214b2c5f44e10945ea33254efcdd
Andre Paul <andrepaul8128@gmail.com>
2021-02-10 16:19:03 -0800

    approval card wraps comments

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:00PM"
-        commentText="Nice blog post!"
-        myAvatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:20PM"
-        commentText="I don't like this."
-        myAvatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 9:35AM"
-        commentText="Where am I?"
-        myAvatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:00PM"
+          commentText="Nice blog post!"
+          myAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:20PM"
+          commentText="I don't like this."
+          myAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 9:35AM"
+          commentText="Where am I?"
+          myAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
