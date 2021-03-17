08094d3e9e630c16b6791434689883e7a0bc1a64
Andre Paul <andrepaul8128@gmail.com>
2021-03-17 14:00:17 -0700

    axios get is working

--- react-redux-prac/pics/src/components/App.js
+++ react-redux-prac/pics/src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get();
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID HzHfuuu7mIj72Wv_CyNanB5ZaSNtK42miohzab42Yu0",
+      },
+    });
   }
 
   render() {
